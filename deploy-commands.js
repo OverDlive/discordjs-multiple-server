c0644e4971d56a40fc535f048e4aefff2591f5aa
allssu <zxcvb6615@gmail.com>
2022-02-05 23:18:01 +0900

    초기 커밋

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,13 +2,13 @@ const fs = require("fs");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
-const { clientId, guildId, token } = require("./config.json");
+const { clientId, guildIds, token } = require("./config.json");
 
 const commandFiles = fs
   .readdirSync("./commands")
   .filter((file) => file.endsWith(".js"));
 
-const commands = [].map((command) => command.toJSON());
+const commands = [];
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
@@ -17,7 +17,25 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: "9" }).setToken(token);
 
-rest
-  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-  .then(() => console.log("커맨드 등록 완료"))
-  .catch(console.error);
+(async () => {
+  guildIds.map(async (guildId) => {
+    try {
+      await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+        body: commands,
+      });
+      console.log(`${guildId} 서버 성공`);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  // 글로벌 명령어 등록 시 주석을 풀고 사용
+  // try {
+  //   await rest.put(Routes.applicationCommands(clientId), {
+  //     body: commands,
+  //   });
+  //   console.log(`글로벌 명령어 등록 성공`);
+  // } catch (error) {
+  //   console.error(error);
+  // }
+})();
